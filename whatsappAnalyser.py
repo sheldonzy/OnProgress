1f719fc32e6a2d4ea6b68fe5bc84c9d3c8d95f81
Tal Sheldon <talsheldon@hotmail.com>
2018-03-12 19:44:59 +0200

    Add files via upload

--- whatsappAnalyser.py
+++ whatsappAnalyser.py
@@ -1,4 +1,6 @@
 import re
+import regex
+import emoji
 import sys
 import dateutil
 import pandas as pd
@@ -6,7 +8,7 @@ import matplotlib.pyplot as plt
 import string
 from collections import Counter, defaultdict
 from bidi import algorithm as bidialg
-
+import HebrewStopWords
 
 date_patterns = {
 #"long_datetime" : "(?P<datetime>\d{1,2}\s{1}\w{3}(\s{1}|\s{1}\d{4}\s{1})\d{2}:\d{2})"
@@ -53,18 +55,64 @@ class Chat:
         y = x.read()
         content = y.splitlines()
         return content
+    
+    
+## Merge and sum given dictionaries
+def dsum(*dicts):
+    ret = defaultdict(int)
+    #add loop for Series of dicts
+    for x in dicts:
+        for d in x:
+            for k, v in d.items():
+                ret[k] += v
+    return dict(ret)
+
+## Merge and sum dictionary of dectionaries
+def dsum_dictionary(dicts):
+    ret = defaultdict(int)
+    #add loop for Series of dicts
+    for x in dicts.values():
+        for k, v in x.items():
+            ret[k] += v
+    return dict(ret)
+    
+def rotate_xticks(ax, rotate):
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(rotate)
+    
+"""
+For a given string:
+    - remove punctuations
+    - return emojis and words counter
+TODO: currently if there are 2 different emojis without space between them, it would count them as well.
+"""
+    
+def split_count(text):
+    table = str.maketrans({key: None for key in string.punctuation})
+    if text:
+        total_emoji = []
+        text = text.translate(table)
+        data = regex.findall(r'\X',text)
+        flag = False
+        for word in data:
+            for char in word:
+                if char in emoji.UNICODE_EMOJI:
+                    flag = True
+            if flag:
+                total_emoji += [word] # total_emoji is a list of all emojis
+                flag = False
+        
+        # Remove from the given text the emojis
+        for current in total_emoji:
+            text = text.replace(current, '') 
+        if text:
+            return Counter(text.split()), Counter(total_emoji)
+        else:
+            return Counter(), Counter(total_emoji)
+    else:
+        return Counter(), Counter()
 
 class Parser:
-    
-    ## Merge and sum given dictionaries
-    def dsum(self, *dicts):
-        ret = defaultdict(int)
-        #add loop for Series of dicts
-        for x in dicts:
-            for d in x:
-                for k, v in d.items():
-                    ret[k] += v
-        return dict(ret)
     
     def parse_message(self,str):
         for pattern in map(lambda x:x+message_pattern, date_patterns.values()):
@@ -120,9 +168,9 @@ class Parser:
 
         df['media'] = ((df['message'] == '<Media omitted>') | (df['message'] == "<מדיה הושמטה>")).astype(int)
         
-        # Remove punctuations and counter words
-        table = str.maketrans({key: None for key in string.punctuation})
-        df['message_counter'] = df['message'].apply(lambda x: Counter((x.translate(table)).split()) if x else Counter())
+        # Remove punctuations, and count all words and emojis for each message
+        df['message_counter'], df['emoji_counter'] = zip(*df['message'].apply(lambda x: split_count(x)))
+        
         
         df_actions = df[pd.isnull(df['message'])]
         df_messages = df[pd.isnull(df['action'])]
@@ -134,8 +182,10 @@ class Parser:
     
     
 
-
 def charts(df):
+    
+
+    
     ## Create Canvas
     fig = plt.figure(figsize=(18,16))
     plt.title = "Whatsapp Analyser"
@@ -145,7 +195,9 @@ def charts(df):
     ax4 = plt.subplot2grid((4,6), (1,0), rowspan=1, colspan=2)
     ax5 = plt.subplot2grid((4,6), (1,2), rowspan=1, colspan=2)
     ax6 = plt.subplot2grid((4,6), (1,4), rowspan=1, colspan=2)    
-    ax7 = plt.subplot2grid((4,6), (2,0), rowspan=2, colspan = 6)
+    ax7 = plt.subplot2grid((4,6), (2,0), rowspan=2, colspan=2)
+    ax8 = plt.subplot2grid((4,6), (2,2), rowspan=2, colspan=2)
+    ax9 = plt.subplot2grid((4,6), (2,4), rowspan=2, colspan=2)
 
 
     ## Create Charts
@@ -158,30 +210,78 @@ def charts(df):
     for label in ax1.xaxis.get_ticklabels()[::2]:
         label.set_visible(False)
     
-    ## Graph 2
-    df.groupby('Day').count().plot(y="message",ax=ax2, kind='bar', legend = None, title = 'Days')
+    ## Graph 2 - number of messages per day
+    df.groupby('Day').count().plot(y="message",ax=ax2, kind='bar', legend = None, title = 'Days', rot=0)
     
-    ## Graph 3
-    messages_by_name = df.name.value_counts()
-    ax3.set_xticklabels([bidialg.get_display(name) for name in messages_by_name.index])
+
+    ## Graph 3 - number of messages per name
+    groupby_name = df.groupby('name')
+    messages_by_name = groupby_name.count()['message']
+    names = [bidialg.get_display(name) for name in messages_by_name.index]
     messages_by_name.plot(ax=ax3, kind = 'bar', title = 'Number of messages')
+    ax3.set_xticklabels(names)    
+    rotate_xticks(ax3, 50)
     
+    ## 4th graph - Visualize message by date in weeks.
+    df.groupby('Time').count()['message'].resample('M').sum().plot(
+            y="message",ax=ax4, legend = None, title = 'Message by Date (in months)')
+    
+    ## 5th graph - media 
+    media = groupby_name.sum()['media']
+    ax5 = pd.Series(data=[val for val in media.values]).plot(kind='bar', ax=ax5, title='Media sharing')
+    ax5.set_xticklabels(names)
+    rotate_xticks(ax5, 50)
+    
+    ## 6th graph - number of words per name
     top_words_count_name = df_messages.groupby('name')['message_counter'].agg(dsum)
     
-    # 6th graph
-    top_all = {}
-    for name, words in top_words_count_name.items():
-        top_all[name] = sum(words.values())
+    values = [(name, sum(words.values())) for name, words in top_words_count_name.items()]
+    ax6 = pd.Series(data =[val[1] for val in values]).plot(kind='bar', ax=ax6, title = 'Number of words')
+    ax6.set_xticklabels(names)
+    rotate_xticks(ax6, 50)
     
-    ax6.bar(list(top_all.keys()), list(top_all.values()))
+    ## 7th graph - most used word and emoji per name
+    emoji_count_name = df_messages.groupby('name')['emoji_counter'].agg(dsum)
     
+    stop_words = HebrewStopWords.getStopWords()
+    # Remove stop words from all words
+    top_words_count_name_clean = {name: {k:v for k,v in count.items() if k not in stop_words} for name, count in top_words_count_name.items()}
+    top_words = {name: Counter(words).most_common(5) for name, words in top_words_count_name_clean.items()}
 
-    # 7th graph - Visualize message by date in weeks.
-    df.groupby('Time').count()['message'].resample('W-Sun').sum().plot(
-            y="message",ax=ax7, legend = None, title = 'Message by Date (in weeks)')
+    names_length = messages_by_name.__len__()
+    names_series = pd.Series(data = [1] * names_length)
+    ax7 = names_series.plot(kind='barh', ax=ax7, title='Top 5 words')
+    topWordsInStrings = ['        '.join([val[0] for val in current]) for current in top_words.values()]
+    for index, p in enumerate(ax7.patches):
+        ax7.annotate(bidialg.get_display(topWordsInStrings[index]), (0.05, p.get_y() + p.get_width() / names_length ))
+    ax7.set_yticklabels([bidialg.get_display(name) for name in top_words.keys()])
+    
+    ## 8th graph - top 10 used words 
+    top_words_all = Counter(dsum_dictionary(top_words_count_name_clean)).most_common(15)
+    top_words_all_series = pd.Series(index=[val[0] for val in top_words_all], data=[val[1] for val in top_words_all])
+    ax8 = top_words_all_series.plot(kind='bar', ax=ax8, title='Top words all')
+    ax8.set_xticklabels([bidialg.get_display(name[0]) for name in top_words_all])
+    rotate_xticks(ax8, 45)
+    
+    ## 9th graph - top 5 used emoji
+    top_emoji = Counter(dsum(emoji_count_name)).most_common(10)
+    top_emoji_series = pd.Series(index=[val[0] for val in top_emoji], data=[val[1] for val in top_emoji])
+    ax9 = top_emoji_series.plot(kind='bar', ax=ax9, title='Top emojis all')
 
+    for label in ax9.get_xticklabels() :
+        label.set_fontproperties("Segoe UI Emoji")
+    rotate_xticks(ax9, 55)
+    
+    # Changing some visuals
+    for ax in fig.axes:
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+    
+    ax7.get_xaxis().set_ticks([])
+    ax7.spines['bottom'].set_visible(False)
     plt.subplots_adjust(wspace=0.46, hspace=1)
     plt.show() 
+    return top_words
     
 
 
@@ -192,7 +292,7 @@ if __name__ == '__main__':
 
     parser = Parser() 
     top, df_messages, df_actions = parser.process(content) 
-    charts(df_messages)
+    a = charts(df_messages)
     
 
 
